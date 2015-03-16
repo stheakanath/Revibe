2f817e0bda547f9c93298dcf36ba8debb8ba4d3d
Sony Theakanath <stheakanath@gmail.com>
2015-03-15 19:27:08 -0700

    added starter files without xibs

---
+++ Revibe/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  Revibe
+//
+//  Created by Sony Theakanath on 3/15/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIViewController
+
+@end
---
+++ Revibe/LoginView.m
@@ -0,0 +1,37 @@
+//
+//  LoginView.m
+//  Revibe
+//
+//  Created by Sony Theakanath on 3/15/15.
+//  Copyright (c) 2015 Sony Theakanath. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@interface LoginView ()
+
+@end
+
+@implementation LoginView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
