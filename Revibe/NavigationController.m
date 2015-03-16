a892d8ab5a384a227f12d15ac36ec1d7939ef893
Sony Theakanath <stheakanath@gmail.com>
2015-03-15 19:24:17 -0700

    finished Navigation Controller. Starting work on Welcome View.

--- Revibe/NavigationController.m
+++ Revibe/NavigationController.m
@@ -6,32 +6,18 @@
 //  Copyright (c) 2015 Sony Theakanath. All rights reserved.
 //
 
+#import "AppConstant.h"
 #import "NavigationController.h"
-
-@interface NavigationController ()
-
-@end
 
 @implementation NavigationController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.navigationBar.barTintColor = HEXCOLOR(0x5BCAEAFF);
+    self.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationBar.titleTextAttributes =
+    @{NSForegroundColorAttributeName:[UIColor whiteColor], NSFontAttributeName:[UIFont fontWithName:@"Avenir-Medium" size:24]};
+    self.navigationBar.translucent = NO;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
